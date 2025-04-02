a6f06eac9438fabcf33b8657888a80b89627a09d
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-04-02 07:20:05 +0100

    allow ssh as root

--- hvssh.py
+++ hvssh.py
@@ -9,8 +9,12 @@ class HVSSH:
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.private_key = paramiko.RSAKey.from_private_key_file(self.creds_handler.ssh.key_path, password=self.creds_handler.ssh.passphrase)
 
-    def run(self, cmd):
-        self.client.connect(hostname=self.hostname, port=22, username=self.creds_handler.ssh.username, pkey=self.private_key)
+    def run(self, cmd, username=None):
+        if not username:
+            # if not username is passed, e.g. "root", 
+            # we SSH as the regular user set in creds.yaml
+            username = self.creds_handler.ssh.username
+        self.client.connect(hostname=self.hostname, port=22, username=username, pkey=self.private_key)
         stdin, stdout, stderr = self.client.exec_command(cmd)
         output = stdout.read().decode('utf-8')
         error = stderr.read().decode('utf-8')
