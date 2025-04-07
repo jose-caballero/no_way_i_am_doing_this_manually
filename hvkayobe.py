016ff96835d81d38371fe1daac782d4ca19411dc
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-04-07 08:27:01 +0100

    adding logger to hvkayobe

--- hvkayobe.py
+++ hvkayobe.py
@@ -1,8 +1,10 @@
 import subprocess
 
+from logger import SetLogger
 
-class HVKayobe:
+class HVKayobe(SetLogger):
     def __init__(self, creds_handler):
+        self._get_logger()
         self.creds_handler = creds_handler
 
     def run(self, cmd):
@@ -14,9 +16,11 @@ class HVKayobe:
              username to ssh to that host
              command to execute
         """
+        self.log.debug('starting run')
         cmd = f"kayobe {self.creds_handler.kayobe.passphrase} {self.creds_handler.kayobe.hostname} {self.creds_handler.kayobe.username} {cmd}"
         subproc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, universal_newlines=True)
         (out, err) = subproc.communicate()
         end_t = datetime.datetime.now()
         st = subproc.returncode
+        self.log.debug('leaving run')
         return out, err, st
