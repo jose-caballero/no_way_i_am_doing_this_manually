fae8a6d6ee4a1305dbf61acef20947febd920dd2
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-04-07 08:26:01 +0100

    adding logger to hvaquilon

--- hvaquilon.py
+++ hvaquilon.py
@@ -1,14 +1,17 @@
 import paramiko
 
+from logger import SetLogger
 
 
-class HVAquilon:
+class HVAquilon(SetLogger):
     def __init__(self, creds_handler):
+        self._set_logger()
         self.creds_handler = creds_handler
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     def run(self, cmd):
+        self.log.debug('starting run')
         self.client.connect(hostname="aquilon.gridpp.rl.ac.uk", username=self.creds_handler.aquilon.username, password=self.creds_handler.aquilon.password)
         aqcmd = "export AQHOST=aquilon.gridpp.rl.ac.uk; export AQSERVICE=aqd;"
         aqcmd += "export PATH=/opt/aquilon/bin/:$PATH;"
@@ -18,6 +21,7 @@ class HVAquilon:
         error = stderr.read().decode('utf-8').strip()
         rc = stdout.channel.recv_exit_status()
         self.client.close()
+        self.log.debug('leaving run')
         return output, error, rc
 
 
