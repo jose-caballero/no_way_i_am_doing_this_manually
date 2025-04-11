88018b620d3eb7252e685acee173b55d1197ec20
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-04-11 07:35:48 +0100

    fix script mellanox

--- scripts/mellanox_playbook.sh
+++ scripts/mellanox_playbook.sh
@@ -7,14 +7,12 @@ LOGFILE="logs.mallanox.${current_datetime}"
 
 source ~/kayobe-prod/env-vars.sh
 ansible-playbook ansible/mellanox-enable-uefi-pxe.yml -i ${HYPERVISOR}, --extra-vars "pxe_target=${HYPERVISOR}" &> $LOGFILE
-cat $LOGFILE | awk -v RS= -v ORS="\n\n" '/^fatal/ { print; next } { last = $0 } END { print last }'
+cat $LOGFILE | awk -v RS= -v ORS="\n\n" '/fatal:/ || /PLAY RECAP/'
 
 #
 #  Explanation of the awk command:
 #
-#   - RS=                       treats paragraphs (blocks separated by blank lines) as records.
-#   - ORS="\n\n"                ensures output paragraphs stay separated.
-#   - /^fatal/ { print; next }  prints any paragraph starting with "fatal" immediately.
-#   - { last = $0 }             saves each paragraph as the potential "last paragraph".
-#   - END { print last }        prints the final paragraph after all input is read.
+#   - RS=                           treats paragraphs (blocks separated by blank lines) as records.
+#   - ORS="\n\n"                    ensures output paragraphs stay separated.
+#   - '/fatal:/ || /PLAY RECAP/'    prints any paragraph including strings "fatal:" or "PLAY RECAP"
 #
