5d143e0bc411b5db0baba0cb3cbe68c57f09f0de
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-04-18 11:52:57 +0100

    wrap alertmanager code in a try except block

--- hvalertmanager.py
+++ hvalertmanager.py
@@ -16,6 +16,17 @@ class HVAlertManager(SetLogger):
 
     def create_silence(self):
         self.log.debug("starting create_silence")
+        try:
+            self._create_silence()
+        except Exception as ex:
+            msg = f'Exception captured: {ex}'
+            self.log.debug(msg)
+            self.jira.add("Exception captured")
+            self.jira.add_block(ex)
+            self.jira.add_comment()
+        self.log.debug('leaving create_silence')
+
+    def _create_silence(self):
         silence_data_hostname = {
             "matchers": [
                 {"name": "hostname",
@@ -38,9 +49,9 @@ class HVAlertManager(SetLogger):
             "createdBy": "admin",
             "comment": f"RL9 Reinstall {self.time_interval.start_str} - JCB"
         }
-        response = self._create_silence(silence_data_hostname)
+        response = self._create_silence_matcher(silence_data_hostname)
         out_msg = f"Silence for hostname: {self.alertmanager_url}/#/silences/{response.json()['silenceID']}"
-        response = self._create_silence(silence_data_instance)
+        response = self._create_silence_matcher(silence_data_instance)
         out_msg += "\n"
         out_msg += f"Silence for instance: {self.alertmanager_url}/#/silences/{response.json()['silenceID']}"
         msg = f"silence created in AlertManager successfully, from {self.time_interval.start_str} to {self.time_interval.end_str}"
@@ -48,39 +59,29 @@ class HVAlertManager(SetLogger):
         msg += out_msg
         self.log.debug(msg)
         self.jira.add_comment(msg)
-        self.log.debug('leaving create_silence')
 
 
-    def _create_silence(self, silence_data):
+    def _create_silence_matcher(self, silence_data):
         silences_endpoint = f"{self.alertmanager_url}/api/v2/silences"
-        try:
-            # Make a POST request to the Alertmanager API with basic auth
-            headers = {"Content-Type": "application/json"}
-            from requests.auth import HTTPBasicAuth
-            basic = HTTPBasicAuth(self.creds_handler.alertmanager.username, self.creds_handler.alertmanager.password)
-            response = requests.post(silences_endpoint, auth=basic, json=silence_data, headers=headers)
+        # Make a POST request to the Alertmanager API with basic auth
+        headers = {"Content-Type": "application/json"}
+        from requests.auth import HTTPBasicAuth
+        basic = HTTPBasicAuth(self.creds_handler.alertmanager.username, self.creds_handler.alertmanager.password)
+        response = requests.post(silences_endpoint, auth=basic, json=silence_data, headers=headers)
     
-            if response.status_code != 200:
-                msg = f"Failed to create silence. Response status code: {response.status_code}"
-                self.log.debug(msg)
-                self.jira.add(msg)
-                text = f"Response text: {response.text}"
-                self.log.debug(text)
-                self.jira.add_block(response.text)
-                self.jira.add_comment()
-            else:
-                msg = f"Silence created successfully"
-                self.log.debug(msg)
+        if response.status_code != 200:
+            msg = f"Failed to create silence. Response status code: {response.status_code}"
+            self.log.debug(msg)
+            self.jira.add(msg)
+            text = f"Response text: {response.text}"
+            self.log.debug(text)
+            self.jira.add_block(response.text)
+            self.jira.add_comment()
+            raise Exception(response.text)
+        else:
+            msg = f"Silence created successfully"
+            self.log.debug(msg)
 
-        except requests.exceptions.RequestException as e:
-            self.log.debug(f"Failed to create silence in Alertmanager: {e}")
-            if e.response is not None:
-                self.log.debug("Response status code:", e.response.status_code)
-                self.log.debug("Response text:", e.response.text)
-            raise e
-        except Exception as e:
-            self.log.debug(f"An error occurred while creating the silence: {e}")
-            raise e
 
 
     def remove_silence(self):
