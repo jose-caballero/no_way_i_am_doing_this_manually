03b2d9049f50a9f17109a75df9ffeb025a4458f8
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-04-23 17:23:29 +0100

    fix bug

--- hvssh.py
+++ hvssh.py
@@ -59,14 +59,14 @@ class HVSSH(SetLogger):
     def ensure_root_access(self):
 
         if self.has_root_access:
-            msg = f"user {self.username} has root acccess to hypervisor {self.hostname}"
+            msg = f"user {self.ssh_username} has root acccess to hypervisor {self.hostname}"
             self.log.debug(msg)
             self.jira.add_comment(msg)
             return
 
         # if not root access...
         # Connect as regular user
-        self.client.connect(self.hostname, username=self.username, key_filename=self.ssh_private_key_path)
+        self.client.connect(self.hostname, username=self.ssh_username, key_filename=self.ssh_private_key_path)
         # Append the public key to /root/.ssh/authorized_keys via sudo
 
         # Read your public SSH key
