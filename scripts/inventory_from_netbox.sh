186f5a6619053b86acea0febc004c30cf8d88db1
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-05-25 11:06:11 +0100

    add scripts

---
+++ scripts/inventory_from_netbox.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+HYPERVISOR=$1
+
+current_datetime=$(date +"%Y_%m_%d_%H_%M")
+LOGFILE="logs_inventory_netbox_${HYPERVISOR}_${current_datetime}"
+
+source ~/kayobe-prod/env-vars.sh
+
+kayobe playbook run ansible/build-inventory-from-netbox.yml &> $LOGFILE
+cat $LOGFILE | awk -v RS= -v ORS="\n\n" '/fatal:/ || /PLAY RECAP/'
+
+#
+#  Explanation of the awk command:
+#
+#   - RS=                           treats paragraphs (blocks separated by blank lines) as records.
+#   - ORS="\n\n"                    ensures output paragraphs stay separated.
+#   - '/fatal:/ || /PLAY RECAP/'    prints any paragraph including strings "fatal:" or "PLAY RECAP"
---
+++ scripts/kayobe_overcloud_deploy_hypervisor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+HYPERVISOR=$1
+
+current_datetime=$(date +"%Y_%m_%d_%H_%M")
+LOGFILE="logs_overcloud_deploy_hypervisor_${HYPERVISOR}_${current_datetime}"
+
+source ~/kayobe-prod/env-vars.sh
+
+kayobe overcloud service deploy -kl $HYPERVISOR --limit $HYPERVISOR &> $LOGFILE
+
+cat $LOGFILE | awk -v RS= -v ORS="\n\n" '/fatal:/ || /PLAY RECAP/'
+
+#
+#  Explanation of the awk command:
+#
+#   - RS=                           treats paragraphs (blocks separated by blank lines) as records.
+#   - ORS="\n\n"                    ensures output paragraphs stay separated.
+#   - '/fatal:/ || /PLAY RECAP/'    prints any paragraph including strings "fatal:" or "PLAY RECAP"
