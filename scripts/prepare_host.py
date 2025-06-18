3398ea773de3f7e419d32831611f4c94f7d4c6a1
Jose Caballero Bejar <jcaballero.hep@gmail.com>
2025-06-18 15:21:38 +0100

    remove no longer needed steps

--- scripts/prepare_host.py
+++ scripts/prepare_host.py
@@ -13,25 +13,24 @@ from myaq.disk import Disk
 hv_name = sys.argv[1]
 hv = Host(hv_name)
 
-
 archetype = Archetype("cloud")
 personality = Personality("kayobe-prod", archetype=archetype)
-os = OperatingSystem("rocky", "9x-x86_64")
-profile = Profile(personality=personality, archetype=archetype, os=os)
-domain = Domain("prod_cloud_hvs")
+#os = OperatingSystem("rocky", "9x-x86_64")
+#profile = Profile(personality=personality, archetype=archetype, os=os)
+#domain = Domain("prod_cloud_hvs")
 
-print(f"manging hypervisor {hv.name} to the right domain")
-results = domain._manage_host(hv)
-print("cmd: ")
-print(results.cmd)
-print("stdout: ")
-print(results.out)
-print("stderr: ")
-print(results.err)
-print("rc: ")
-print(results.rc)
-if results.rc != 0:
-    sys.exit()
+#print(f"manging hypervisor {hv.name} to the right domain")
+#results = domain._manage_host(hv)
+#print("cmd: ")
+#print(results.cmd)
+#print("stdout: ")
+#print(results.out)
+#print("stderr: ")
+#print(results.err)
+#print("rc: ")
+#print(results.rc)
+#if results.rc != 0:
+#    sys.exit()
 
 if "a100" in hv_name:
     disk = Disk("sda")
@@ -47,7 +46,8 @@ if "a100" in hv_name:
     if results.rc != 0:
         sys.exit()
 
-results = hv.make_profile(profile)
+#results = hv.make_profile(profile)
+results = hv.make(personality=personality)
 print("cmd: ")
 print(results.cmd)
 print("stdout: ")
@@ -70,5 +70,4 @@ print("rc: ")
 print(results.rc)
 if results.rc != 0:
     sys.exit()
-
 
